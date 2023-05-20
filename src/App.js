6c89955949eab5095b2616b300c18bc6ebe129aa
Alpha <rohithss2001@gmail.com>
2023-05-20 11:11:55 +0530

    Todo App Created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+ const[toDo,setToDo]=useState([])
+ const[toDos,setToDos]=useState("")
   return (
     <div className="app">
       <div className="mainHeading">
@@ -12,19 +14,53 @@ function App() {
         <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
       </div>
       <div className="input">
-        <input type="text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
+        <input value={toDos} onChange={(e)=>setToDos([
+          e.target.value
+        ])} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>setToDo(
+          [...toDo,{id:Date.now(),text:toDos,status:false}]
+        )} className="fas fa-plus"></i>
       </div>
       <div className="todos">
-        <div className="todo">
-          <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>Rect tutorial</p>
+       {
+        toDo.map((obj)=>{
+          return(
+            <div className="todo">
+            <div className="left">
+              <input value={obj.status} onChange={(e)=>{
+                console.log(e.target.checked)
+                console.log(obj)
+                setToDo(toDo.filter(obj2=>{
+                  if(obj2.id===obj.id){
+                    obj2.status=e.target.checked
+                  }
+                  return obj2
+                }))
+                  
+                
+              }} type="checkbox" name="" id="" />
+              <p>{obj.text}</p>
+            </div>
+            <div className="right">
+              <i className="fas fa-times"></i>
+            </div>
           </div>
-          <div className="right">
-            <i className="fas fa-times"></i>
-          </div>
-        </div>
+          )
+        }
+        )
+       }
+
+        {
+          toDo.map((obj) => {
+            if (obj.status) {
+              return (
+                <h1>{obj.text}</h1>
+              )
+            }
+            return null
+        })
+       }
+          
       </div>
     </div>
   );
